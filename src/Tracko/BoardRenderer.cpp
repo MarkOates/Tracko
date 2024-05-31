87329957b77db5678b4d9982c484fec1ae6ff0a8
Mark Oates <markoates0@gmail.com>
2024-05-31 12:20:56 -0400

    * Add basic BoardRenderer class

--- src/Tracko/BoardRenderer.cpp
+++ src/Tracko/BoardRenderer.cpp
@@ -63,27 +63,20 @@ void BoardRenderer::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("BoardRenderer::render: error: guard \"board\" not met");
    }
-   std::string quote = "Tracko";
-   float x = 1920/2;
-   float y = 1080/3;
-   ALLEGRO_FONT *font = obtain_font();
-   float text_width = al_get_text_width(font, quote.c_str());
-   float text_height = al_get_font_line_height(font);
-   float h_text_width = text_width/2;
-   float h_text_height = text_height/2;
-   AllegroFlare::Vec2D padding = {30, 20};
+   int num_rows = board->get_num_rows();
+   int num_columns = board->get_num_columns();
+   float row_height = 80;
+   float column_width = 80;
 
-   al_draw_rounded_rectangle(
-      x-h_text_width - padding.x,
-      y-h_text_height - padding.y,
-      x+h_text_width + padding.x,
-      y+h_text_height + padding.y,
-      8.0f,
-      8.0f,
-      ALLEGRO_COLOR{1, 1, 1, 1},
-      8.0f
-   );
-   al_draw_text(font, ALLEGRO_COLOR{1, 1, 1, 1}, x, y-h_text_height, ALLEGRO_ALIGN_CENTER, quote.c_str());
+   for (int row=0; row<num_rows; row++)
+   {
+      for (int column=0; column<num_columns; column++)
+      {
+         float x = row * row_height;
+         float y = column * column_width;
+         al_draw_filled_circle(x, y, 4, ALLEGRO_COLOR{0.2, 0.21, 0.22, 0.22});
+      }
+   }
    return;
 }
 
