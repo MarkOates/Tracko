6122bfb6cfda5e702192720a09d5acdf0b0404ea
Mark Oates <markoates0@gmail.com>
2024-05-31 11:07:52 -0400

    * Add basic Piece with state

--- include/Tracko/Piece.hpp
+++ include/Tracko/Piece.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 
-#include <string>
+#include <allegro5/allegro.h>
+#include <cstdint>
 
 
 namespace Tracko
@@ -9,6 +10,17 @@ namespace Tracko
    class Piece
    {
    private:
+      enum State
+      {
+         STATE_UNDEF = 0,
+         STATE_HIDDEN,
+         STATE_REVEALED,
+         STATE_FILLING,
+         STATE_FILLED,
+      };
+      uint32_t state;
+      bool state_is_busy;
+      float state_changed_at;
 
    protected:
 
@@ -17,7 +29,12 @@ namespace Tracko
       Piece();
       ~Piece();
 
-      std::string run();
+      uint32_t get_state() const;
+      void set_state(uint32_t state=STATE_UNDEF, bool override_if_busy=false);
+      void update_state(float time_now=al_get_time());
+      static bool is_valid_state(uint32_t state=STATE_UNDEF);
+      bool is_state(uint32_t possible_state=STATE_UNDEF);
+      float infer_current_state_age(float time_now=al_get_time());
    };
 }
 
--- src/Tracko/Piece.cpp
+++ src/Tracko/Piece.cpp
@@ -2,7 +2,12 @@
 
 #include <Tracko/Piece.hpp>
 
-
+#include <AllegroFlare/Logger.hpp>
+#include <allegro5/allegro.h>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <stdexcept>
 
 
 namespace Tracko
@@ -10,6 +15,9 @@ namespace Tracko
 
 
 Piece::Piece()
+   : state(STATE_UNDEF)
+   , state_is_busy(false)
+   , state_changed_at(0.0f)
 {
 }
 
@@ -19,9 +27,109 @@ Piece::~Piece()
 }
 
 
-std::string Piece::run()
+uint32_t Piece::get_state() const
 {
-   return "Hello World!";
+   return state;
+}
+
+
+void Piece::set_state(uint32_t state, bool override_if_busy)
+{
+   if (!(is_valid_state(state)))
+   {
+      std::stringstream error_message;
+      error_message << "[Piece::set_state]: error: guard \"is_valid_state(state)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("Piece::set_state: error: guard \"is_valid_state(state)\" not met");
+   }
+   if (this->state == state) return;
+   if (!override_if_busy && state_is_busy) return;
+   uint32_t previous_state = this->state;
+
+   switch (state)
+   {
+      case STATE_HIDDEN:
+      break;
+
+      case STATE_REVEALED:
+      break;
+
+      case STATE_FILLING:
+      break;
+
+      case STATE_FILLED:
+      break;
+
+      default:
+         AllegroFlare::Logger::throw_error(
+            "ClassName::set_state",
+            "Unable to handle case for state \"" + std::to_string(state) + "\""
+         );
+      break;
+   }
+
+   this->state = state;
+   state_changed_at = al_get_time();
+
+   return;
+}
+
+void Piece::update_state(float time_now)
+{
+   if (!(is_valid_state(state)))
+   {
+      std::stringstream error_message;
+      error_message << "[Piece::update_state]: error: guard \"is_valid_state(state)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("Piece::update_state: error: guard \"is_valid_state(state)\" not met");
+   }
+   float age = infer_current_state_age(time_now);
+
+   switch (state)
+   {
+      case STATE_HIDDEN:
+      break;
+
+      case STATE_REVEALED:
+      break;
+
+      case STATE_FILLING:
+      break;
+
+      case STATE_FILLED:
+      break;
+
+      default:
+         AllegroFlare::Logger::throw_error(
+            "ClassName::update_state",
+            "Unable to handle case for state \"" + std::to_string(state) + "\""
+         );
+      break;
+   }
+
+   return;
+}
+
+bool Piece::is_valid_state(uint32_t state)
+{
+   std::set<uint32_t> valid_states =
+   {
+      STATE_HIDDEN,
+      STATE_REVEALED,
+      STATE_FILLING,
+      STATE_FILLED,
+   };
+   return (valid_states.count(state) > 0);
+}
+
+bool Piece::is_state(uint32_t possible_state)
+{
+   return (state == possible_state);
+}
+
+float Piece::infer_current_state_age(float time_now)
+{
+   return (time_now - state_changed_at);
 }
 
 
