de7a1fbc24790a6fe43d85e48acbbbdf4df98ff2
Mark Oates <markoates0@gmail.com>
2024-06-01 16:50:45 -0400

    * Remove some dead tests

--- tests/Tracko/Visuals/TrackPathTest.cpp
+++ tests/Tracko/Visuals/TrackPathTest.cpp
@@ -29,47 +29,6 @@ TEST_F(Tracko_Visuals_TrackPathTest, render__without_allegro_initialized__raises
 }
 
 
-/*
-TEST_F(Tracko_Visuals_TrackPathTest, render__without_primitives_addon_initialized__raises_an_error)
-{
-   al_init();
-   Tracko::Visuals::TrackPath track_path;
-   std::string expected_error_message =
-      "TrackPath::render: error: guard \"al_is_primitives_addon_initialized()\" not met";
-   EXPECT_THROW_WITH_MESSAGE(track_path.render(), std::runtime_error, expected_error_message);
-   al_uninstall_system();
-}
-
-
-TEST_F(Tracko_Visuals_TrackPathTest, render__without_font_addon_initialized__raises_an_error)
-{
-   al_init();
-   al_init_primitives_addon();
-   Tracko::Visuals::TrackPath track_path;
-   std::string expected_error_message =
-      "TrackPath::render: error: guard \"al_is_font_addon_initialized()\" not met";
-   EXPECT_THROW_WITH_MESSAGE(track_path.render(), std::runtime_error, expected_error_message);
-   al_shutdown_primitives_addon();
-   al_uninstall_system();
-}
-
-
-TEST_F(Tracko_Visuals_TrackPathTest, render__without_a_font_bin__raises_an_error)
-{
-   al_init();
-   al_init_primitives_addon();
-   al_init_font_addon();
-   Tracko::Visuals::TrackPath track_path;
-   std::string expected_error_message =
-      "TrackPath::render: error: guard \"font_bin\" not met";
-   EXPECT_THROW_WITH_MESSAGE(track_path.render(), std::runtime_error, expected_error_message);
-   al_shutdown_font_addon();
-   al_shutdown_primitives_addon();
-   al_uninstall_system();
-}   
-*/
-
-
 TEST_F(Tracko_Visuals_TrackPathTestWithAllegroRenderingFixture, CAPTURE__render__will_not_blow_up)
 {
    AllegroFlare::Camera2D camera;
